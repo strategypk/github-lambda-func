f062d0c8bffc7189eb4e158af2aae738d9587641
strategypk <strategypk@gmail.com>
2023-07-21 15:23:09 -0700

    stock of the trade updated

--- lambda_function.py
+++ lambda_function.py
@@ -1,7 +1,7 @@
 import json
 def lambda_handler(event, context):
     data=json.dumps(event)
-    downloadlink = "https://finance.yahoo.com/quote/TSLA"
+    downloadlink = "https://finance.yahoo.com/quote/MSFT"
     return {'statusCode': 200, 'headers': {'Content-Type': 'application/json'}, 'body': json.dumps(downloadlink)}
 
-print('Recommended Stock of the Day July 21 sec : TSLA')
+print('Recommended Stock of the Day July 21 sec : MSFT')
