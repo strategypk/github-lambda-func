39be37b43eeac8369714aaa50241c283392b2a7c
strategypk <strategypk@gmail.com>
2023-07-21 16:13:47 -0700

    update

--- lambda_function.py
+++ lambda_function.py
@@ -4,7 +4,7 @@ def lambda_handler(event, context):
     downloadlink = "https://finance.yahoo.com/quote/NFLX"
     return {'statusCode': 200, 'headers': {'Content-Type': 'application/json'}, 'body': json.dumps(downloadlink)}
 
-print('Recommended Stock of the Day July 21 sec : MSFT')
+print('Recommended Stock of the Day July 21 sec : NFLX')
 # downloadlink = "https://finance.yahoo.com/quote/TSLA"
 # downloadlink = "https://finance.yahoo.com/quote/AMZN"
 # downloadlink = "https://finance.yahoo.com/quote/MSFT"
