771cab2e622e366bc18bdd892a5579efb9c6d961
strategypk <strategypk@gmail.com>
2023-07-21 14:50:27 -0700

    buildspec

--- lambda_function.py
+++ lambda_function.py
@@ -1,7 +1,7 @@
 import json
 def lambda_handler(event, context):
     data=json.dumps(event)
-    downloadlink = "https://finance.yahoo.com/quote/TSLA?p=TSLA"
+    downloadlink = "https://finance.yahoo.com/quote/TSLA"
     return {'statusCode': 200, 'headers': {'Content-Type': 'application/json'}, 'body': json.dumps(downloadlink)}
 
 print('Recommended Stock of the Day July 21 : TSLA')
