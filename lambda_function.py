eb1952d4101cf948d9b77b4f0313a78219b48898
strategypk <strategypk@gmail.com>
2023-07-22 07:26:14 -0700

    top stock today

--- lambda_function.py
+++ lambda_function.py
@@ -1,10 +1,12 @@
 import json
 def lambda_handler(event, context):
     data=json.dumps(event)
-    downloadlink = "https://finance.yahoo.com/quote/NFLX"
+    downloadlink = "https://finance.yahoo.com/quote/NVDA"
     return {'statusCode': 200, 'headers': {'Content-Type': 'application/json'}, 'body': json.dumps(downloadlink)}
 
 print('Recommended Stock of the Day July 21 sec : NFLX')
 # downloadlink = "https://finance.yahoo.com/quote/TSLA"
 # downloadlink = "https://finance.yahoo.com/quote/AMZN"
 # downloadlink = "https://finance.yahoo.com/quote/MSFT"
+# downloadlink = "https://finance.yahoo.com/quote/NFLX"
+# downloadlink = "https://finance.yahoo.com/quote/NVDA"
