293df8fba754b728ca0bbc449fe4e4dd9c06d971
strategypk <strategypk@gmail.com>
2023-07-22 10:13:16 -0700

    crm print statement

--- lambda_function.py
+++ lambda_function.py
@@ -4,7 +4,7 @@ def lambda_handler(event, context):
     downloadlink = "https://finance.yahoo.com/quote/CRM"
     return {'statusCode': 200, 'headers': {'Content-Type': 'application/json'}, 'body': json.dumps(downloadlink)}
 
-print('Recommended Stock of the Day July 21 sec : NFLX')
+print('Recommended Stock of the Day July 21 sec : CRM')
 # downloadlink = "https://finance.yahoo.com/quote/TSLA"
 # downloadlink = "https://finance.yahoo.com/quote/AMZN"
 # downloadlink = "https://finance.yahoo.com/quote/MSFT"
