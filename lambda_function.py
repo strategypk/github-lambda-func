25da595d3bed02e27e474416f288fdac04000654
strategypk <strategypk@gmail.com>
2023-07-23 09:28:22 -0700

    gitup update msft

--- lambda_function.py
+++ lambda_function.py
@@ -1,10 +1,10 @@
 import json
 def lambda_handler(event, context):
     data=json.dumps(event)
-    downloadlink = "https://finance.yahoo.com/quote/CRM"
+    downloadlink = "https://finance.yahoo.com/quote/MSFT"
     return {'statusCode': 200, 'headers': {'Content-Type': 'application/json'}, 'body': json.dumps(downloadlink)}
 
-print('Recommended Stock of the Day July 21 sec : CRM')
+print('Recommended Stock of the Day July 21 sec : MSFT')
 # downloadlink = "https://finance.yahoo.com/quote/TSLA"
 # downloadlink = "https://finance.yahoo.com/quote/AMZN"
 # downloadlink = "https://finance.yahoo.com/quote/MSFT"
